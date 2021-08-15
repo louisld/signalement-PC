1b2f8f7437278511c19d38f8e10808db47b1434b
louisld <louisldthesamui@gmail.com>
2021-08-15 23:31:26 +0200

    Mise à jour du wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-import sys, os, pwd
+import sys
+import os
+
+from signalement import create_app
 
 project = "signalement"
 
@@ -6,5 +9,4 @@ BASE_DIR = os.path.join(os.path.dirname(__file__))
 if BASE_DIR not in sys.path:
     sys.path.append(BASE_DIR)
 
-from signalement import create_app
 application = create_app()
